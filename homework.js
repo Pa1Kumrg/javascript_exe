5d074c775df72d54f7c50283a9c836c65ec2e19e
Pa1Kumrg <golipavan90@gmail.com>
2023-04-05 21:31:37 -0500

    adding homework file

---
+++ homework.js
@@ -0,0 +1,5 @@
+subjects=["maths","science", "social","telugu", "hindi","english"]
+hours=["morning","afternoon","evening"]
+
+classhours=[...subjects,...hours]
+console.log("classhours",classhours);
